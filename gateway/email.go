f7ce0e10d7c5490de5ab670e045f3f97d274a164
MCarbono <carbono.marcelo@gmail.com>
2023-06-11 19:17:09 -0300

    chore: creating mock email gateway

--- gateway/email.go
+++ gateway/email.go
@@ -2,6 +2,7 @@ package gateway
 
 import "lenslocked/domain/entity"
 
+//go:generate mockgen -destination=../gen/mock/gateway_email_mock.go -package=mock . EmailProvider
 type EmailProvider interface {
 	Send(email *entity.Email) error
 }
