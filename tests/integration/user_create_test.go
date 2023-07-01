ea449d839e7c49a36081a6a4d56ad65e68cc2750
MCarbono <carbono.marcelo@gmail.com>
2023-07-01 14:58:54 -0300

    test: new case integation create user

---
+++ tests/integration/user_create_test.go
@@ -0,0 +1,59 @@
+package integration
+
+import (
+	"lenslocked/application/usecases"
+	repository "lenslocked/infra/repository/sqlite"
+	"lenslocked/tests/fakes"
+	"lenslocked/tests/testinfra"
+	"os/exec"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestCreateUser(t *testing.T) {
+	t.Cleanup(func() {
+		cmd := exec.Command("rm", "../lenslocked_test.db")
+		err := cmd.Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+	db, err := testinfra.CreateDatabaseTest()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+	var userRepository = repository.NewUserRepositorySQLite(db)
+	var creteUserUseCase = usecases.NewCreateUserUseCase(userRepository, fakes.NewIDGeneratorFake())
+
+	type test struct {
+		name  string
+		input *usecases.CreateUserInput
+	}
+
+	tests := []test{
+		{
+			name: "Should create a new user",
+			input: &usecases.CreateUserInput{
+				Email:    "user@email.com",
+				Password: "password",
+			},
+		},
+	}
+	for _, scenario := range tests {
+		t.Run(scenario.name, func(t *testing.T) {
+			got, err := creteUserUseCase.Execute(scenario.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			want, err := userRepository.FindByID(got.ID)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf("Create Gallery mismatch (-want +got):\n%v", diff)
+			}
+		})
+	}
+}
