2c5bc46a22655e4d7d1fd25522e714ba3da9c82c
MCarbono <carbono.marcelo@gmail.com>
2023-07-05 20:23:50 -0300

    feat: image repository FInd method

--- infra/repository/disk/imageRepository.go
+++ infra/repository/disk/imageRepository.go
@@ -7,6 +7,7 @@ import (
 	"lenslocked/domain/entity"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type ImageRepositoryDisk struct {
@@ -22,7 +23,18 @@ func NewImageRepositoryDisk(path string, extensions []string) *ImageRepositoryDi
 }
 
 func (r *ImageRepositoryDisk) Find(galleryID string) ([]*entity.Image, error) {
-	return nil, nil
+	globPattern := filepath.Join(r.galleryDir(galleryID), "*")
+	allFiles, err := filepath.Glob(globPattern)
+	if err != nil {
+		return nil, fmt.Errorf("retrieving gallery images: %w", err)
+	}
+	var images []*entity.Image
+	for _, file := range allFiles {
+		if r.hasExtension(file) {
+			images = append(images, entity.NewImage(galleryID, file, filepath.Base(file)))
+		}
+	}
+	return images, nil
 }
 
 func (r *ImageRepositoryDisk) FindOne(galleryID, filename string) (*entity.Image, error) {
@@ -39,4 +51,15 @@ func (r *ImageRepositoryDisk) FindOne(galleryID, filename string) (*entity.Image
 
 func (r *ImageRepositoryDisk) galleryDir(galleryID string) string {
 	return filepath.Join(r.path, fmt.Sprintf("gallery-%s", galleryID))
+}
+
+func (r *ImageRepositoryDisk) hasExtension(file string) bool {
+	for _, ext := range r.extensions {
+		file = strings.ToLower(file)
+		ext = strings.ToLower(ext)
+		if filepath.Ext(file) == ext {
+			return true
+		}
+	}
+	return false
 }
