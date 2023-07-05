e92346ed9d734277559a222144b8de246dc1750b
MCarbono <carbono.marcelo@gmail.com>
2023-07-05 20:24:08 -0300

    refact: findGallery usecase with repository

--- application/usecases/findGallery.go
+++ application/usecases/findGallery.go
@@ -4,18 +4,17 @@ import (
 	"fmt"
 	"lenslocked/application/repository"
 	"lenslocked/domain/entity"
-	"path/filepath"
-	"strings"
 )
 
 type FindGalleryUseCase struct {
 	galleryRepository repository.GalleryRepository
-	imagesDir         string
+	imageRepository   repository.ImageRepository
 }
 
-func NewFindGalleryUseCase(galleryRepository repository.GalleryRepository) *FindGalleryUseCase {
+func NewFindGalleryUseCase(galleryRepository repository.GalleryRepository, imageRepository repository.ImageRepository) *FindGalleryUseCase {
 	return &FindGalleryUseCase{
 		galleryRepository: galleryRepository,
+		imageRepository:   imageRepository,
 	}
 }
 
@@ -24,40 +23,10 @@ func (uc *FindGalleryUseCase) Execute(ID string) (*entity.Gallery, error) {
 	if err != nil {
 		return nil, fmt.Errorf("query gallery by ID %w", err)
 	}
-	globPattern := filepath.Join(uc.galleryDir(ID), "*")
-	allFiles, err := filepath.Glob(globPattern)
+	images, err := uc.imageRepository.Find(gallery.ID)
 	if err != nil {
-		return nil, fmt.Errorf("retrieving gallery images: %w", err)
-	}
-	var images []*entity.Image
-	for _, file := range allFiles {
-		if uc.hasExtension(file, uc.extensions()) {
-			images = append(images, entity.NewImage(gallery.ID, file, filepath.Base(file)))
-		}
+		return nil, err
 	}
 	gallery.AddImages(images)
 	return gallery, nil
-}
-
-func (uc *FindGalleryUseCase) galleryDir(ID string) string {
-	imagesDir := uc.imagesDir
-	if imagesDir == "" {
-		imagesDir = "images"
-	}
-	return filepath.Join(imagesDir, fmt.Sprintf("gallery-%s", ID))
-}
-
-func (uc *FindGalleryUseCase) hasExtension(file string, extensions []string) bool {
-	for _, ext := range extensions {
-		file = strings.ToLower(file)
-		ext = strings.ToLower(ext)
-		if filepath.Ext(file) == ext {
-			return true
-		}
-	}
-	return false
-}
-
-func (uc *FindGalleryUseCase) extensions() []string {
-	return []string{".png", ".jpg", ".jpeg", ".gif"}
 }
